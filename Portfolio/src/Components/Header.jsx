e24922c243875b9f41419077f5dc0993c2b1aa93
nkovlev <nkovlev4@gmail.com>
2023-06-20 21:37:14 +0300

    homesection

--- Portfolio/src/Components/Header.jsx
+++ Portfolio/src/Components/Header.jsx
@@ -1,13 +1,38 @@
+import { useState } from 'react';
 import logo from '../images/logo.svg';
+import {IoMdClose} from 'react-icons/io'
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className="absolute w-full">
-      <div className="flex items-center gap-4">
-        <img src={logo} alt="logo" className='w-16 h-16'/>
-        <h2 className='text-purple-700 text-2xl font-poppins font-extrabold'>.Nick</h2>
+    <header className="absolute w-full flex items-center justify-between">
+      <div className="flex items-center gap-4 ml-10">
+        <img src={logo} alt="logo" className="w-16 h-16" />
+        <h2 className="text-purple-700 text-2xl font-poppins font-extrabold">.Nick</h2>
       </div>
-      
+      <div className="relative mr-10" onClick={handleMenuToggle}>
+        <div className="h-0.5 w-8 bg-white mb-1"></div>
+        <div className="h-0.5 w-8 bg-white mb-1"></div>
+        <div className="h-0.5 w-8 bg-white mb-1"></div>
+      </div>
+        {isOpen && (
+          <div className="fixed top-0 right-0 h-screen w-1/3 bg-black shadow-lg z-10">
+            <div className="absolute right-0 mt-5 mr-10 text-white" onClick={handleMenuToggle}>
+              <IoMdClose className="w-8 h-8"/>
+            </div>
+            <ul className="py-2 px-4 space-y-2 text-white">
+              <li>Menu Item 1</li>
+              <li>Menu Item 2</li>
+              <li>Menu Item 3</li>
+              <li>Menu Item 4</li>
+            </ul>
+          </div>
+        )}
     </header>
   );
 }
--- Portfolio/src/Components/HomeSection.jsx
+++ Portfolio/src/Components/HomeSection.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from 'react';
 import Typed from 'typed.js';
+import me from '../images/2023-06-20 18.21.35.png'
+import {BsArrowDown} from 'react-icons/bs'
 
 const HomeSection = () => {
 const typedRef = useRef(null);
@@ -8,7 +10,7 @@ useEffect(() => {
     const options = {
         strings: ['Mykyta Kovlev','Junior', 'Frontend', 'Developer'],
         typeSpeed: 150,
-        backSpeed: 100,
+        backSpeed: 40,
         loop: true,
     };
 
@@ -20,13 +22,20 @@ useEffect(() => {
 }, []);
 
 return (
-    <div className="w-screen h-screen">
-        <div className="flex justify-center items-center w-full h-full">
-            <h1 className="text-white text-5xl font-poppins font-extrabold">
-                Hi, I am <span className="typed-text text-purple-700"></span>
-            </h1>
+<div className="w-screen h-screen flex flex-col justify-center items-center">
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-5">
+        <div className="w-40 h-40 bg-purple-700 rounded-full flex items-center justify-center">
+            <img src={me} alt="me" className="w-36 h-36 rounded-full" />
         </div>
+        <h1 className="text-white text-5xl font-poppins font-extrabold">
+            Howdy, I am <span className="typed-text text-purple-700"></span>
+        </h1>
+        <p className="text-white text-xl">I am a Trainee / Junior Frontend Developer from <span className="text-yellow-400">Ukraine</span>.</p>
     </div>
+    <div className="mb-10">
+        <p className="text-white flex items-center gap-2 hover:text-purple-700"><BsArrowDown className="animate-bounce"/> Scroll Down</p>
+    </div>
+</div>
 );
 };
 
