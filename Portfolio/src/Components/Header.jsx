47dc9e25f7154c39b48bdcdb07ecae19a7d34a7e
nkovlev <nkovlev4@gmail.com>
2023-06-21 09:12:57 +0300

    homesection

--- Portfolio/src/Components/Header.jsx
+++ Portfolio/src/Components/Header.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 import logo from '../images/logo.svg';
-import {IoMdClose} from 'react-icons/io'
+import { IoMdClose } from 'react-icons/io';
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const menuItems = ['Home', 'About', 'Projects', 'Contacts'];
+  const [selectedItem, setSelectedItem] = useState('Home');
+
   const handleMenuToggle = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleMenuItemClick = (item) => {
+    setSelectedItem(item);
+    setIsOpen(false);
   };
 
   return (
@@ -20,19 +28,25 @@ function Header() {
         <div className="h-0.5 w-8 bg-white mb-1"></div>
         <div className="h-0.5 w-8 bg-white mb-1"></div>
       </div>
-        {isOpen && (
-          <div className="fixed top-0 right-0 h-screen w-1/3 bg-black shadow-lg z-10">
-            <div className="absolute right-0 mt-5 mr-10 text-white" onClick={handleMenuToggle}>
-              <IoMdClose className="w-8 h-8"/>
-            </div>
-            <ul className="py-2 px-4 space-y-2 text-white">
-              <li>Menu Item 1</li>
-              <li>Menu Item 2</li>
-              <li>Menu Item 3</li>
-              <li>Menu Item 4</li>
-            </ul>
-          </div>
-        )}
+      <div
+        className={`fixed top-0 right-0 h-screen w-1/3 bg-zinc-900 shadow-lg z-10 ${ isOpen ? 'menu-open' : 'menu-closed' }`}>
+        <div className="absolute right-0 mt-5 mr-10 text-white" onClick={handleMenuToggle}>
+          <IoMdClose className="w-8 h-8" />
+        </div>
+        <ul className="pl-16 pt-40 space-y-6 text-white">
+          {menuItems.map((item, index) => (
+            <li
+              key={index}
+              onClick={() => handleMenuItemClick(item)}
+              className={`font-semibold text-3xl hover:text-white ${
+                selectedItem === item ? 'text-white' : 'text-gray-400'
+              }`}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 }
